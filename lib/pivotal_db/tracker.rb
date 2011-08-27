f23e1194513f9c725b0cbf5726185aece0dd51ce
Fraser Newton <fraser@goclio.com>
2011-08-27 17:15:02 -0600

    spec example

---
+++ lib/pivotal_db/tracker.rb
@@ -0,0 +1,9 @@
+module PivotalDb
+  class Tracker
+    
+    def self.example
+      "example"
+    end
+
+  end
+end
---
+++ spec/pivotal_db_spec.rb
@@ -0,0 +1,8 @@
+require 'pivotal_db/tracker'
+
+describe PivotalDb::Tracker do
+  it "example test" do
+    PivotalDb::Tracker.example.should eql("example")
+  end
+
+end
